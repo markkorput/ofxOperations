eb80175d59962b24083fb363dbfb2416fd940817
Mark van de Korput <mark.korput@gmail.com>
2017-05-12 11:05:32 +0200

    case sensitive typo

--- src/ofxOperations.h
+++ src/ofxOperations.h
@@ -3,5 +3,5 @@
 #include "ofxOperations/Operation.h"
 #include "ofxOperations/OperationGroup.h"
 #include "ofxOperations/Generator.h"
-#include "ofxOperations/Params/Generator.h"
+#include "ofxOperations/params/Generator.h"
 #include "ofxOperations/gui/Launcher.h"
